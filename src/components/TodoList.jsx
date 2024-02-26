f7bf4defa17363ec63586662eee1726f63db46c7
RomainAuger7 <romain.auger.jd@gmail.com>
2024-02-26 16:23:16 +0900

    Modified way to fetch todos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,31 +1,16 @@
 import React from "react"
 import TodoItem from "./TodoItem"
-import { getUser } from "@/lib/data"
-
-const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/todos", {
-    next: { revalidate: 3600 },
-  })
-
-  if (!res.ok) {
-    throw new Error("Something went wrong")
-  }
-
-  return res.json()
-}
+import { getTodos, getUser } from "@/lib/data"
 
 const TodoList = async ({ session }) => {
   const user = await getUser(session.user.email)
   const activeUserId = user[0]._id
-
-  const todos = await getData()
-  const displayTodos = todos.filter(
-    (todo) => todo.userId === activeUserId.toString()
-  )
+  const todos = await getTodos(activeUserId)
+  console.log(todos)
 
   return (
     <div className="flex flex-col p-5 justify-between gap-2">
-      {displayTodos.map((todo) => (
+      {todos.map((todo) => (
         <TodoItem key={todo._id} todo={todo} />
       ))}
     </div>
