016b111a30c64d07cca393d92222c464930f895f
RomainAuger7 <romain.auger.jd@gmail.com>
2024-03-05 11:56:26 +0900

    updating db uri

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -15,7 +15,7 @@ export const connecttoDb = async () => {
       console.log("Using existing connection")
       return
     }
-    const db = await mongoose.connect(process.env.MONGO_DB)
+    const db = await mongoose.connect(process.env.MONGODB_URI)
     connection.isConnected = db.connections[0].readyState
   } catch (error) {
     console.log(error)
